d0fc9c6b23b7e953340a83ab2e4eacb466fa8165
Dũng Phạm <dungpham259.dev@gmail.com>
2026-06-22 21:54:14 +0700

    chore: scaffold KMP repo and gradle skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# pos-version-kit-kmp
+
+Kotlin Multiplatform version-check/download/install kit for VTcode POS (Android + Desktop).
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "pos-version-kit-kmp"
+include(":version-kit-mp")
